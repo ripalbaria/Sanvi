2edc18576950ee4b8bdb32b0f0cf41dc89fbd040
ripalbaria <63595505+ripalbaria@users.noreply.github.com>
2026-01-13 17:42:41 +0530

    Update merge_script.py

--- merge_script.py
+++ merge_script.py
@@ -3,7 +3,7 @@ import json
 
 # Your URLs
 M3U_URLS = [
-    "https://raw.githubusercontent.com/ripalbaria/Zeewillow/refs/heads/main/zee5_updated.m3u",
+    "https://raw.githubusercontent.com/cloudplay97/m3u/refs/heads/main/zee5-worldwide.m3u",
     "https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/jtv.m3u"
 ]
 JSON_URL = "https://raw.githubusercontent.com/ripalbaria/play/refs/heads/main/Slivtv.json"
